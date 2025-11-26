77d10a24881abd2fa9b46cb7c3c184a69732ee9e
nakkala nandakishore <nakkalanandakishore@gmail.com>
2025-11-26 11:50:23 +0530

    Add files via upload

---
+++ average of the five subjects marks.c
@@ -0,0 +1,10 @@
+/*write a c program to print average of five subjects marks*/
+#include <stdio.h>
+int main()
+{
+	int a,b,c,d,e,avg;
+	printf("enter the value of a,b,c,d,e ");
+	scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&e);
+	avg=(a+b+c+d+e)/5;
+	printf("the average is %d",avg);
+}
